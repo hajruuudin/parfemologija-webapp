c7d99698237c496cc302374de5c8c4923ec0116e
Hajrudin Imamovic <hajruuudin@gmail.com>
2025-05-05 22:42:51 +0200

    BE: Accord CRUD no documentation

--- parfemologija-be/src/main/java/ba/parfemologija/service/implementation/mapper/AccordMapper.java
+++ parfemologija-be/src/main/java/ba/parfemologija/service/implementation/mapper/AccordMapper.java
@@ -3,7 +3,9 @@ package ba.parfemologija.service.implementation.mapper;
 import ba.parfemologija.dao.entities.AccordEntity;
 import ba.parfemologija.service.core.models.accord.AccordCreate;
 import ba.parfemologija.service.core.models.accord.AccordModel;
+import ba.parfemologija.service.core.models.accord.AccordUpdate;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 import java.util.List;
 
@@ -12,5 +14,5 @@ public interface AccordMapper {
     List<AccordModel> entitiesToDtos(List<AccordEntity> entities);
     AccordModel entityToDto(AccordEntity entity);
     AccordEntity dtoToEntity(AccordCreate accordCreate);
-
+    void update(AccordUpdate accordUpdate, @MappingTarget AccordEntity accordEntity);
 }
