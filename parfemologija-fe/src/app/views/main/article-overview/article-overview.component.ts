0c2c51a47042646c2d93941536899cc34fbbbf76
emirsultan1 <emir.sultan@stu.ibu.edu.ba>
2025-06-13 07:52:20 -0700

    article_overview

--- parfemologija-fe/src/app/views/main/article-overview/article-overview.component.ts
+++ parfemologija-fe/src/app/views/main/article-overview/article-overview.component.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgIf, NgFor, CurrencyPipe } from '@angular/common';
+import { NgxSpinnerComponent, NgxSpinnerService } from 'ngx-spinner';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { ArticleService } from '../../../controller/article.service';
+import { ArticleModel }   from '../../../model/article-model';
 
 @Component({
+  standalone: true,
   selector: 'app-article-overview',
-  imports: [],
+  imports: [
+    NgIf,
+    NgFor,
+    CurrencyPipe,
+    NgxSpinnerComponent
+  ],
   templateUrl: './article-overview.component.html',
-  styleUrl: './article-overview.component.css'
+  styleUrls: ['./article-overview.component.css'],
+  host: {
+    class: 'max-body-width w-full flex flex-col md:flex-row md:m-auto justify-center md:space-x-2 items-start'
+  }
 })
-export class ArticleOverviewComponent {
+export class ArticleOverviewComponent implements OnInit {
+  article: ArticleModel | null = null;
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private articleService: ArticleService,
+    private spinner: NgxSpinnerService
+  ) {}
+
+  ngOnInit(): void {
+    this.spinner.show();
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.articleService.getArticleById(id).subscribe(
+        (res: ArticleModel) => {
+          this.article = res;
+        },
+        (err: any) => {
+          console.error(err);
+        },
+        () => {
+          this.spinner.hide();
+        }
+      );
+    } else {
+      this.spinner.hide();
+    }
+  }
+
+  goBack(): void {
+    this.router.navigate(['/articles']);
+  }
 }
